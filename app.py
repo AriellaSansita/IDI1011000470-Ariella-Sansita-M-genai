6b6b976a24bfc3481dd0d371a4440dc15ddd3ac6
Ariella Sansita <ariella.sansita@gmail.com>
2026-02-14 16:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import random
 
 # ---------------- CONFIG ----------------
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 model = genai.GenerativeModel(
     "gemini-2.5-flash",
     generation_config={
